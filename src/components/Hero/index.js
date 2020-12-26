ddab1881b68ec64699c2465b34e894c1fbb82ea1
Theja Rasanjalee <ugthejaugamage@gmail.com>
2020-12-26 13:15:31 +0530

    Navbar

---
+++ src/components/Hero/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../NavBar'
+import {HeroCotainer} from './HeroElements'
+const Hero=() =>{
+    return (
+        <HeroCotainer>
+            <Navbar/>
+        </HeroCotainer>
+    )
+}
+
+export default Hero
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Nav, NavLink, NavIcon,Bars} from './NavbarElements'
+
+const Navbar = () => {
+    return (
+        <div>
+           <Nav>
+               <NavLink to='/'>pixxxdd</NavLink>
+             
+< NavIcon >  menu<Bars /></ NavIcon>
+
+           </Nav>
+        </div>
+    )
+}
+
+export default Navbar
